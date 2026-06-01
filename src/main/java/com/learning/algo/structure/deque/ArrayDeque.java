3c0c8f2547882d3d488d69dc83d9be40c9dff2e3
luc-dev10 <luc.dev1008@gmail.com>
2026-06-01 08:55:13 -0400

    Adds initial changes

---
+++ src/main/java/com/learning/algo/structure/deque/ArrayDeque.java
@@ -0,0 +1,28 @@
+package com.learning.algo.structure.deque;
+
+public class ArrayDeque {
+  int[] array = new int[3];
+  int size = 0;
+
+  public void add(int value) {
+    this.array[size++] = value;
+  }
+
+  public int removeFront() {
+    if (this.size == 0) return -1;
+
+    int temp = this.array[0];
+
+    for (int i = 0; i < this.size - 1; i++) {
+      this.array[i] = this.array[i + 1];
+    }
+
+    this.size--;
+    return temp;
+  }
+
+  public int removeBack() {
+    if (this.size == 0) return -1;
+    return this.array[--this.size];
+  }
+}
