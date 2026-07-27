eefa047d77ce8f3253da7b33b4e21cede27748d1
luciozhao <zhaolucio@gmail.com>
2026-07-26 21:07:30 -0400

    dsa - adds Stack ds

--- src/main/java/com/lucio/dsa/ds/stack/ArrayStack.java
+++ src/main/java/com/lucio/dsa/ds/stack/ArrayStack.java
@@ -1,15 +1,37 @@
 package com.lucio.dsa.ds.stack;
 
-public class ArrayStack {
+public final class ArrayStack {
+  private final int LIMIT = 3;
   int[] array = new int[3];
   int size = 0;
 
+  public ArrayStack() {
+    this.array = new int[this.LIMIT];
+    this.size = 0;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public boolean isFull() {
+    return this.size == this.LIMIT;
+  }
+
+  public int peek() {
+    return this.isEmpty() ? -1 : this.array[this.size - 1];
+  }
+
   public int pop() {
-    if (this.size == 0) return -1;
-    return this.array[--this.size];
+    return this.isEmpty() ? -1 : this.array[--this.size];
   }
 
   public void push(int value) {
+    if (this.isFull()) return;
     this.array[this.size++] = value;
   }
 }
