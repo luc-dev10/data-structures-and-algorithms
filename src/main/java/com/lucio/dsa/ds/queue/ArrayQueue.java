faeb28ab10b2364403db87aac64f466fa85d61d3
luciozhao <zhaolucio@gmail.com>
2026-07-27 19:12:02 -0400

    dsa - adds Array Queue ds

--- src/main/java/com/lucio/dsa/ds/queue/ArrayQueue.java
+++ src/main/java/com/lucio/dsa/ds/queue/ArrayQueue.java
@@ -1,23 +1,46 @@
 package com.lucio.dsa.ds.queue;
 
 public class ArrayQueue {
-  int[] array = new int[3];
-  int size = 0;
+  private final int CAPACITY = 3;
+  private final int[] array;
+  private int size;
+
+  public ArrayQueue() {
+    this.array = new int[CAPACITY];
+    this.size = 0;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public boolean isFull() {
+    return this.size == this.CAPACITY;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public int peek() {
+    return this.isEmpty() ? -1 : this.array[0];
+  }
 
   public void enqueue(int value) {
+    if (this.isFull()) return;
     this.array[size++] = value;
   }
 
   public int dequeue() {
     if (size == 0) return -1;
-
     int temp = this.array[0];
 
     for (int i = 0; i < this.size - 1; i++) {
       this.array[i] = this.array[i + 1];
     }
 
-    this.size--; // this.array[--this.size] = -1;
+    this.array[this.size - 1] = -1;
+    this.size--;
     return temp;
   }
 }
