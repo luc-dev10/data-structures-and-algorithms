1ccd29d0e0ea78e8b3ea71d06a4bbddae40969df
luciozhao <zhaolucio@gmail.com>
2026-08-02 00:07:35 -0400

    algo - adds Linear Search

---
+++ src/main/java/com/lucio/dsa/algo/search/LinearSearch.java
@@ -0,0 +1,17 @@
+package com.lucio.dsa.algo.search;
+
+public class LinearSearch {
+  public static void main(String[] args) {
+    int[] array = new int[] {5, 1, 5, 2, 9, 2, 4, 6, 7, 34, 12, 66, 8};
+    int target = 6;
+    System.out.printf("Linear Search for Target %d: %d\n", target, search(array, target));
+  }
+
+  public static int search(int[] array, int target) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == target) return i;
+    }
+
+    return -1;
+  }
+}
